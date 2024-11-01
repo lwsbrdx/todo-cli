d0705a1e23120e769ff1f7aa395b91f3e483806f
lwsbrdx <lewisberdix@gmail.com>
2024-11-01 20:47:56 +0100

    feat: list trashed projects

--- commands/project_list.go
+++ commands/project_list.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"todo/flags"
 	"todo/models"
 	"todo/renderers"
 	"todo/services"
@@ -13,10 +14,17 @@ var ListProjects = cli.Command{
 	Name:    "list",
 	Aliases: []string{"l"},
 	Usage:   "List projects",
-	Action:  listProjects,
+	Flags: []cli.Flag{
+		flags.NewTrashedFlag(),
+	},
+	Action: listProjects,
 }
 
 func listProjects(cCtx *cli.Context) error {
+	if cCtx.Bool("trashed") {
+		return listTrashedProjects(cCtx)
+	}
+
 	projects := []models.Project{}
 
 	if err := services.DbInstance.Db.Find(&projects).Error; err != nil {
@@ -31,5 +39,24 @@ func listProjects(cCtx *cli.Context) error {
 	pr := renderers.ProjectsRenderer{Projects: projects}
 	pr.Render()
 
+	return nil
+}
+
+func listTrashedProjects(_ *cli.Context) error {
+	var projects []models.Project
+	if err := services.DbInstance.Db.
+		Unscoped().
+		Where("deleted_at IS NOT NULL").
+		Find(&projects).Error; err != nil {
+		return err
+	}
+
+	if len(projects) == 0 {
+		fmt.Println("Projects trash is empty")
+		return nil
+	}
+
+	pr := renderers.ProjectsRenderer{Projects: projects}
+	pr.Render()
 	return nil
 }
